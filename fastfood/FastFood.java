2895461c1680a820543bf556308426ed16ccd765
hothithuytien <hothithuytien210719@gmail.com>
2025-03-25 20:34:00 +0700

    Add files via upload

---
+++ fastfood/FastFood.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fastfood;
+
+/**
+ *
+ * @author PC
+ */
+public class FastFood {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+            Login login =     new Login();
+            login.setVisible(true);
+            login.setLocationRelativeTo(null);
+            
+        
+    
+    }
+    
+}
