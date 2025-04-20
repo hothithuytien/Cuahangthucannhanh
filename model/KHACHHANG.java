e59106913d393e25b087249013e16ec38ee78ecf
hothithuytien <hothithuytien210719@gmail.com>
2025-04-20 14:32:15 +0700

    Add files via upload

---
+++ model/KHACHHANG.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author BAO TRAN
+ */
+public class KHACHHANG {
+    private String MAKH;
+    private String HOTENKH;
+    private String GIOITINH;
+    private String SDT;
+    private String DIACHI;
+
+    public KHACHHANG() {
+    }
+
+    public KHACHHANG(String MAKH, String HOTENKH, String GIOITINH, String SDT, String DIACHI) {
+        this.MAKH = MAKH;
+        this.HOTENKH = HOTENKH;
+        this.GIOITINH = GIOITINH;
+        this.SDT = SDT;
+        this.DIACHI = DIACHI;
+    }
+
+    public String getMAKH() {
+        return MAKH;
+    }
+
+    public void setMAKH(String MAKH) {
+        this.MAKH = MAKH;
+    }
+
+    public String getHOTENKH() {
+        return HOTENKH;
+    }
+
+    public void setHOTENKH(String HOTENKH) {
+        this.HOTENKH = HOTENKH;
+    }
+
+    public String getGIOITINH() {
+        return GIOITINH;
+    }
+
+    public void setGIOITINH(String GIOITINH) {
+        this.GIOITINH = GIOITINH;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getDIACHI() {
+        return DIACHI;
+    }
+
+    public void setDIACHI(String DIACHI) {
+        this.DIACHI = DIACHI;
+    }
+}
---
+++ model/NHANVIEN.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+/**
+ *
+ * @author BAO TRAN
+ */
+public class NHANVIEN {
+    private String MANV;
+    private String HOTENNV;
+    private String GIOITINH;
+    private String SDT;
+    private String DIACHI;
+    private String NGAYSINH;
+    
+    public NHANVIEN(){
+    }
+    
+    public NHANVIEN(String MANV, String HOTENNV, String GIOITINH, String SDT, String DIACHI, String NGAYSINH){
+        this.MANV = MANV;
+        this.HOTENNV = HOTENNV;
+        this.GIOITINH = GIOITINH;
+        this.SDT = SDT;
+        this.DIACHI = DIACHI;
+        this.NGAYSINH = NGAYSINH;
+    }
+    
+    public String getMANV() {
+        return MANV;
+    }
+
+    public void setMANV(String MANV) {
+        this.MANV = MANV;
+    }
+
+    public String getHOTENNV() {
+        return HOTENNV;
+    }
+
+    public void setHOTENNV(String HOTENNV) {
+        this.HOTENNV = HOTENNV;
+    }
+
+    public String getGIOITINH() {
+        return GIOITINH;
+    }
+
+    public void setGIOITINH(String GIOITINH) {
+        this.GIOITINH = GIOITINH;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getDIACHI() {
+        return DIACHI;
+    }
+
+    public void setDIACHI(String DIACHI) {
+        this.DIACHI = DIACHI;
+    }
+
+    public String getNGAYSINH() {
+        return NGAYSINH;
+    }
+
+    public void setNGAYSINH(String NGAYSINH) {
+        this.NGAYSINH = NGAYSINH;
+    }   
+}
